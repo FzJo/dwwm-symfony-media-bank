e42079280300d762506ec9df7f46fb06b6362f08
JoFz <43348746+FzJo@users.noreply.github.com>
2020-04-22 20:14:20 +0200

    Maintain controllers

--- src/Controller/Movie/MovieController.php
+++ src/Controller/Movie/MovieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Movie;
 
+use App\Service\MovieListService;
+use App\Service\MovieService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,11 +12,12 @@ class MovieController extends AbstractController
     /**
      * @Route("/movie/{id<tt\d{7}>}", name="movie_id", methods={"GET", "POST"})
      */
-    public function show($id)
+    public function show($id, MovieService $movieService)
     {
-        var_dump($id);
+        $movie = $movieService->getContent($id);
         return $this->render('movie/show.html.twig', [
-            'controller_name' => 'MovieController',
+            'movie' => $movie,
         ]);
     }
+
 }
--- src/Controller/Movies/MoviesController.php
+++ src/Controller/Movies/MoviesController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller\Movies;
 
+use App\Entity\Movie;
+use App\Service\MovieListService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 class MoviesController extends AbstractController
 {
@@ -11,20 +16,23 @@ class MoviesController extends AbstractController
      * @Route("/movies", name="movies", methods={"GET", "POST"})
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function showAll()
+    public function showAll(MovieListService $movieListService)
     {
         return $this->render('movies/show_all.html.twig', [
-            'controller_name' => 'MoviesController',
+            'movies_category' => [
+                'Box Office' => $movieListService->getContent($movieListService::BOX_OFFICE),
+                'Trending' => $movieListService->getContent($movieListService::TRENDING)
+            ],
         ]);
     }
+
     /**
      * @Route("/movies/{genre<action|horror|sci-fi|comedy>}", name="genre", methods={"GET", "POST"})
      */
     public function sortByGenre($genre)
     {
-        var_dump($genre);
-        return $this->render('movies/genre.html.twig', [
-            'controller_name' => 'MoviesController',
+        return $this->render('movies/sort_by_genre.html.twig', [
+            'genre' => $genre
         ]);
     }
 
